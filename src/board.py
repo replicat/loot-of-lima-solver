424f42dfb4a242a2f0921c309ece5d5d75a93257
Lucas Lee <replicat@users.noreply.github.com>
2022-05-28 16:31:42 +0800

    import pulp

--- src/board.py
+++ src/board.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
+import pulp as pl
+
 
 class LootOfLimaBoard:
     def __init__(self, info: list | None = None):
         self.players: tuple = ("a", "b", "c", "d", "e", "public", "loot")
 
         self._info: list = info or []
-        self._problem = None
+        self._problem = pl.LpProblem()
 
     def add_info(self, info: list):
         self._info.append(info)
